895539989e986dbdd174ed88d608bb7badb7af08
Muhammad Abdul Sami <msami.bese22seecs@student.nust.edu.pk>
2023-11-16 00:54:24 +0500

    Update ec2_terraform.tf

--- ec2_terraform.tf
+++ ec2_terraform.tf
@@ -13,3 +13,24 @@ provider "aws" {
   access_key = "AWS_ACCESS_KEY"
   secret_key = "AWS_SECRET_KEY"
 }
+
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+}
+
+
