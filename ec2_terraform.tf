dc3c03d7bbeb5288c696ffad6bc1a91f4fc80689
Muhammad Abdul Sami <msami.bese22seecs@student.nust.edu.pk>
2023-11-16 00:56:24 +0500

    Update ec2_terraform.tf

--- ec2_terraform.tf
+++ ec2_terraform.tf
@@ -33,4 +33,41 @@ resource "local_file" "private_key" {
   filename = var.key_name
 }
 
+# Create a security group
+resource "aws_security_group" "sg_ec2" {
+  name        = "sg_ec2"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "public_instance" {
+  ami                    = "ami-0f5ee92e2d63afc18"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+
+  tags = {
+    Name = "public_instance"
+  }
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+  }
+}
+
+
 
